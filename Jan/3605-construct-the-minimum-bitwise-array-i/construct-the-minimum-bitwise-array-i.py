5051c1891fd6a3c59a9620fa719a98b524d14cf0
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-01-20 23:39:08 +0530

    Time: 159 ms (25.00%) | Memory: 12.5 MB (25.00%) - LeetSync

---
+++ Jan/3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minBitwiseArray(self, nums):
+        ans = []
+
+        for num in nums:
+            is_found = False
+            for i in range(num):
+                if i | (i + 1) == num:
+                    ans.append(i)
+                    is_found = True
+                    break
+
+            if not is_found:
+                ans.append(-1)
+
+        return ans
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        

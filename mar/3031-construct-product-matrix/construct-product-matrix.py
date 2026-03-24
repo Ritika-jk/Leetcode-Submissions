ea8582eff5bfbbbed705c6c3af843051db31339a
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-03-24 23:11:50 +0530

    Time: 176 ms (75.86%) | Memory: 38.3 MB (89.66%) - LeetSync

---
+++ mar/3031-construct-product-matrix/construct-product-matrix.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def constructProductMatrix(self, grid):
+        m=len(grid)
+        n=len(grid[0])
+        lr=[] 
+        for i in grid:
+            for j in i:
+                lr.append(j)
+        rl=list(lr)
+        for i in range(1,len(lr)):
+            lr[i]*=lr[i-1]%12345
+        for i in range(len(rl)-2,-1,-1):
+            rl[i]*=rl[i+1]%12345
+        for i in range(m):
+            for j in range(n):
+                ind=i*n+j
+                if(ind==0):
+                    grid[i][j]=rl[1]%12345
+                elif(ind==m*n-1):
+                    grid[i][j]=lr[-2]%12345
+                else:
+                    grid[i][j]=(lr[ind-1]*rl[ind+1])%12345
+        return grid
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        

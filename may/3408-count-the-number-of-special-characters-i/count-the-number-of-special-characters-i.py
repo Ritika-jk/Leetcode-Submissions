61b5344a45d42e8e4f3b511742804d80b22d4a83
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-05-26 18:42:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.98%) - LeetSync

---
+++ may/3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numberOfSpecialChars(self, word):
+        low = [False] * 26
+        up = [False] * 26
+
+        for c in word:
+            # Lowercase character
+            if 'a' <= c <= 'z':
+                low[ord(c) - ord('a')] = True
+
+            # Uppercase character
+            else:
+                up[ord(c) - ord('A')] = True
+
+        ans = 0
+
+        # Count characters present in both
+        for i in range(26):
+            if low[i] and up[i]:
+                ans += 1
+
+        return ans
+        """
+        :type word: str
+        :rtype: int
+        """
+        

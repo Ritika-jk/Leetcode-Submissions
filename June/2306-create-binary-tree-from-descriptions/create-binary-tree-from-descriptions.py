27957a5ff1f79642dd39aed7b3de3922a44cb4ab
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-06-08 00:02:39 +0530

    Time: 359 ms (16.39%) | Memory: 26 MB (11.48%) - LeetSync

---
+++ June/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def createBinaryTree(self, descriptions):
+        mp = {}
+        root = {}
+
+        for parent, child, isLeft in descriptions:
+
+            if parent not in mp:
+                mp[parent] = TreeNode(parent)
+
+            if child not in mp:
+                mp[child] = TreeNode(child)
+
+            if isLeft == 1:
+                mp[parent].left = mp[child]
+            else:
+                mp[parent].right = mp[child]
+
+            if root.get(parent, 0) != -1:
+                root[parent] = 1
+
+            root[child] = -1
+
+        root_val = 0
+
+        for node, state in root.items():
+            if state == 1:
+                root_val = node
+                break
+
+        return mp[root_val]
+        """
+        :type descriptions: List[List[int]]
+        :rtype: Optional[TreeNode]
+        """
+        

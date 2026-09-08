9ff421018b810cea9c77b6cd7590f75fdbdc73f3
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-09-08 18:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (17.48%) - LeetSync

--- June/4245-count-commas-in-range/count-commas-in-range.py
+++ June/4245-count-commas-in-range/count-commas-in-range.py
@@ -1,9 +1,10 @@
 
 class Solution(object):
     def countCommas(self, n):
-        if n <= 999:
+        if n < 999:
             return 0
-        return n - 999 
+        return n-999
+        
         """
         :type n: int
         :rtype: int

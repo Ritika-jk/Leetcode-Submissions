e3efe48561b0c97cd26377feee9590ea1f48a421
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-09-21 22:26:16 +0530

    Time: 560 ms (66.67%) | Memory: 21.4 MB (0.00%) - LeetSync

---
+++ practice/3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def resultArray(self, A, k):
+        res = freq = [0] * k
+
+        for n in A:
+            n %= k
+            cur = [0] * k
+            cur[n] = 1
+
+            for x, y in enumerate(freq):
+                cur[x * n % k] += y
+
+            freq = cur
+            for x, y in enumerate(freq):
+                res[x] += y
+
+        return res
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        

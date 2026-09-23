c49b8cce3257e4395f880917e97b3d9b6b909295
RITIKA SHARMA <146440022+Ritika-jk@users.noreply.github.com>
2026-09-23 23:35:14 +0530

    Time: 75 ms (96.75%) | Memory: 20.7 MB (58.44%) - LeetSync

---
+++ practice/1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def minOperations(self, nums, x):
+        n = len(nums)
+        s = sum(nums)
+
+        t = s - x
+
+        if t == 0:
+            return n
+
+        if t < 0:
+            return -1
+
+        l = 0
+        w = 0
+        best = -1
+
+        for r in range(n):
+            w += nums[r]
+
+            while w > t:
+                w -= nums[l]
+                l += 1
+
+            if w == t:
+                best = max(best, r - l + 1)
+
+        return -1 if best == -1 else n - best
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+        
